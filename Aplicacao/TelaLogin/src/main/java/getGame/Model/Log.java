8d52bbbc9e9b166cec565332cd2929ef4aab45e9
eduhkbr <edu.abreu09@hotmail.com>
2019-11-08 19:20:05 -0300

    log da aplicacao

---
+++ Aplicacao/TelaLogin/src/main/java/getGame/Model/Log.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package getGame.Model;
+
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.util.Util;
+import org.apache.log4j.Logger;
+/**
+ *
+ * @author Aluno
+ */
+public class Log {
+    
+    public void logando() {
+        Logger log = Logger.getLogger("game");
+        
+        log.info("ERROR DE REGINO CORNO");
+    }
+}
