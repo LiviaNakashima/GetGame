9fc40bc0416cad4e897646f6fbe6b92ff70ffd48
Saulo Oliveira <sauloeniefi@gmail.com>
2019-11-14 20:57:20 -0300

    Criacao dos graficos de hardware

---
+++ Site/public/views/js/demo/chartCpu.js
@@ -0,0 +1,33 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myCpuChart");
+var myCpuChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Disponível", "Indisponível"],
+    datasets: [{
+      data: [70, 30],
+      backgroundColor: ['#d19532', '#858796']
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 70,
+  },
+});
