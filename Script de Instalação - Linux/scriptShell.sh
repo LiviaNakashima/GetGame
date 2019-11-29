96fb2622bc3ee8dd4ce9747f4ef5f1c04240a685
regin0 <regino.trindadeneto@bandtec.com.br>
2019-11-28 23:17:43 -0300

    Adicionando os slides da Lívia e o Script de Instalação

---
+++ Script de Instalação - Linux/scriptShell.sh
@@ -0,0 +1,6 @@
+# ! /bin/bash
+
+echo "Meu primeiro shell script"
+echo "mostra o diretório corrente" pwd
+echo "lista os diretórios e arquivos do diretório de forma detalhada" ls
+java -jar cerberus.jar
