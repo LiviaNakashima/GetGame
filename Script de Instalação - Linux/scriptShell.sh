511abaeaa1ae625937621352e6fd1c13fca91217
regin0 <regino.trindadeneto@bandtec.com.br>
2019-12-03 21:26:01 -0200

    atualizando o script de instalação linux

--- Script de Instalação - Linux/scriptShell.sh
+++ Script de Instalação - Linux/scriptShell.sh
@@ -1,6 +1,12 @@
 # ! /bin/bash
 
 echo "Meu primeiro shell script"
-echo "mostra o diretório corrente" pwd
-echo "lista os diretórios e arquivos do diretório de forma detalhada" ls
-java -jar TelaLogin-1.0-SNAPSHOT-jar-with-dependencies.jar
+echo "mostra o diretório corrente" | 
+pwd |
+echo "lista os diretórios e arquivos do diretório de forma detalhada" |
+ls |
+which java | grep -q /usr/bin/java
+if [ $? = 0 ]
+	then java java -jar TelaLogin-1.0-SNAPSHOT-jar-with-dependencies.jar
+else "JDK não está instalado, por favor faça o download para poder executar nosso projeto"
+fi
